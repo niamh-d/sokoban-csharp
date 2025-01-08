c27f2c133ebd4cc92817fd8712c586a038f5060d
Niamh <niamh.linguist@gmail.com>
2025-01-08 21:26:17 +0200

    game over ui

---
+++ Scenes/GameOverUI/GameOverUi.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+public partial class GameOverUi : Control
+{
+	[Export] private Label _recordLabel;
+	[Export] private Label _movesLabel;
+
+	public override void _Ready()
+	{
+		SignalManager.Instance.OnLevelCompleted += OnLevelCompleted;
+		SignalManager.Instance.OnNewGame += OnNewGame;
+	}
+
+	public override void _ExitTree()
+	{
+		SignalManager.Instance.OnLevelCompleted -= OnLevelCompleted;
+		SignalManager.Instance.OnNewGame -= OnNewGame;
+	}
+
+	private void OnNewGame(string levelNumberStr)
+	{
+		Hide();
+		_recordLabel.Hide();
+	}
+
+	private void OnLevelCompleted(string levelNumberStr, int moves, bool isBest)
+	{
+		Show();
+		_movesLabel.Text = $"{moves} Moves Taken";
+		// record label
+	}
+}
